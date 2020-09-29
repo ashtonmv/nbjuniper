22f8cbe2875799e82115f471b42c2df5fc4a9569
Michael Ashton <mashton@ufl.edu>
2020-09-29 19:44:09 +0200

    Refactor

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
 from distutils.core import setup
 setup(
-  name = 'nbjuniper',         # How you named your package folder (MyLib)
-  packages = ['nbjuniper'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Convert Jupyter Notebooks into runnable HTML files with Juniper',   # Give a short description about your library
-  author = 'Michael Ashton',                   # Type in your name
-  author_email = 'ashtonmv@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/ashtonmv/nbjuniper',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/ashtonmv/nbjuniper/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['jupyter', 'website', 'html', 'notebook', 'interactive', 'juniper'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  name = 'nbjuniper',
+  packages = ['nbjuniper'],
+  version = '0.1.2',
+  license='MIT',
+  description = 'Convert Jupyter Notebooks into runnable HTML files with Juniper',
+  author = 'Michael Ashton',
+  author_email = 'ashtonmv@gmail.com',
+  url = 'https://github.com/ashtonmv/nbjuniper',
+  download_url = 'https://github.com/ashtonmv/nbjuniper/archive/v_012.tar.gz',
+  keywords = ['jupyter', 'website', 'html', 'notebook', 'interactive', 'juniper'],
+  install_requires=[ 
           'pyyaml',
           'markdown',
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.7',
   ],
 )
